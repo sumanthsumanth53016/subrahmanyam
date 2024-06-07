30a4d5eb988bb5709144bab46a24b728b964a895
medida subrahmanyam swami <msubrahmanyamswami@gmail.com>
2024-06-07 22:19:09 +0530

    Add files via upload

---
+++ 0025.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+main()
+{
+
+	int a,b,total;
+	a=b=total=0;
+	printf("enter the price of 1kg rice: ");
+	scanf("%d",&a);
+	printf("enter the quantity of rice :  ");
+	scanf("%d",&b);
+	total=a*b*(1);
+	printf("total is: %d",total);
+	//new line
+	
+}
+
